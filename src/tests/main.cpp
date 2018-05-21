2ac266c69ad72ab1fc7586fe3dba69dbe65db649
Sergey Yurchik <srg91.snz@gmail.com>
2018-05-22 00:34:44 +0300

    start adding boost

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,4 +1,12 @@
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <iostream>
+using namespace std;
 
 int main() {
+    auto tag = boost::uuids::random_generator()();
+
+    cout << "Hello: " << tag << endl;
     return 0;
 }
